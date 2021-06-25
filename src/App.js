f5152640a702096b4904b76f95887e477a0df1b1
Anshu Raj <anshusopinion@gmail.com>
2021-06-25 21:14:36 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { Container, Flex, Stack, List } from "@chakra-ui/layout";
-import Player from "./components/Player";
-import { Heading } from "@chakra-ui/react";
+import { Container, Flex, Heading, List, Stack } from "@chakra-ui/react";
+import { useState } from "react";
 import { useDrop } from "react-dnd";
-const App = () => {
+import Player from "./components/Player";
+
+function App() {
   const [players, setPlayer] = useState([
     { name: "Player 1" },
     { name: "Player 2" },
@@ -14,16 +14,58 @@ const App = () => {
 
   const [team, setTeam] = useState([]);
 
+  const [{ isOver }, addToTeamRef] = useDrop({
+    accept: "player",
+    collect: (monitor) => ({ isOver: !!monitor.isOver() }),
+  });
+
+  console.log(isOver);
+  const [{ isOver: isPlayerOver }, removeFromTeamRef] = useDrop({
+    accept: "team",
+    collect: (monitor) => ({ isOver: !!monitor.isOver() }),
+  });
+
+  const movePlayerToTeam = (item) => {
+    console.log(item);
+    setPlayer((prev) => prev.filter((_, i) => item.index !== i));
+    setTeam((prev) => [...prev, item]);
+  };
+  const removePlayerFromTeam = (item) => {
+    setTeam((prev) => prev.filter((_, i) => item.index !== i));
+    setPlayer((prev) => [...prev, item]);
+  };
+
   return (
     <Container maxW="800px">
-      <Flex justify="space-between" align="center" minH="90vh">
+      <Heading p="2" align="center" color="GrayText">
+        React Drag & Drop
+      </Heading>
+
+      <Flex justify="space-between" height="90vh" align="center">
         <Stack width="300px">
           <Heading fontSize="3xl" color="yellow.800" textAlign="center">
             PLAYERS
           </Heading>
-          <List p="4" minH="70vh" boxShadow="xl" borderRadius="md">
-            {players.map((e, i) => (
-              <Player index={i} />
+          <List
+            bgGradient={
+              isPlayerOver
+                ? "linear(to-b, yellow.300, yellow.500)"
+                : "linear(to-b, yellow.100, yellow.200)"
+            }
+            ref={removeFromTeamRef}
+            p="4"
+            minH="70vh"
+            boxShadow="xl"
+            borderRadius="md"
+          >
+            {players.map((p, i) => (
+              <Player
+                item={p}
+                key={i}
+                playerType="player"
+                onDropPlayer={movePlayerToTeam}
+                index={i}
+              />
             ))}
           </List>
         </Stack>
@@ -31,15 +73,32 @@ const App = () => {
           <Heading fontSize="3xl" color="teal.800" textAlign="center">
             TEAM
           </Heading>
-          <List p="4" minH="70vh" boxShadow="xl" borderRadius="md">
-            {team.map((e, i) => (
-              <Player />
+          <List
+            bgGradient={
+              isOver
+                ? "linear(to-b, teal.300, teal.500)"
+                : "linear(to-b, teal.100, teal.200)"
+            }
+            ref={addToTeamRef}
+            minH="70vh"
+            boxShadow="xl"
+            borderRadius="md"
+            p="4"
+          >
+            {team.map((p, i) => (
+              <Player
+                item={p}
+                key={i}
+                index={i}
+                playerType="team"
+                onDropPlayer={removePlayerFromTeam}
+              />
             ))}
           </List>
         </Stack>
       </Flex>
     </Container>
   );
-};
+}
 
 export default App;
--- src/components/Player.js
+++ src/components/Player.js
@@ -2,10 +2,40 @@ import { ListItem } from "@chakra-ui/react";
 import React from "react";
 import { useDrag } from "react-dnd";
 
-const Player = () => {
+const Player = ({ item, playerType, onDropPlayer, index }) => {
+  const [{ isDragging }, dragRef] = useDrag({
+    type: playerType,
+    item: () => ({ ...item, index }),
+    end: (item, monitor) => {
+      const dropResult = monitor.getDropResult();
+
+      if (item && dropResult) {
+        onDropPlayer(item);
+      }
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
   return (
-    <ListItem p="2" borderRadius="md" boxShadow="md" mb="2" textAlign="center">
-      Player
+    <ListItem
+      p="2"
+      borderRadius="md"
+      boxShadow="md"
+      mb="2"
+      textAlign="center"
+      ref={dragRef}
+      bg={
+        isDragging
+          ? playerType === "player"
+            ? "yellow.600"
+            : "teal.400"
+          : "white"
+      }
+      color={isDragging ? "white" : "black"}
+    >
+      {item.name}
     </ListItem>
   );
 };
